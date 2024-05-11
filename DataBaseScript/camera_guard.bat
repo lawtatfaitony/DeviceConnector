a87b7fc62ad086d8760be0055619db589aaffb34
TonyLaw <lawtatfaitony@gmail.com>
2024-05-12 00:41:08 +0800

    back up mysql dataase bat file

---
+++ DataBaseScript/camera_guard.bat
@@ -0,0 +1,2 @@
+mysqldump -uroot -pAdmin@62595738 camera_guard_business>C:/DataBaseBAK/camera_guard_business.dump
+mysqldump -uroot -pAdmin@62595738 camera_guard_history>C:/DataBaseBAK/camera_guard_history.dump
